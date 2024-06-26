b40ae8f8f82b098f2063b6c684c967c9d00b7ee2
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-26 13:04:34 +0000

    Lesson 1

---
+++ Lesson 1/simplequeries.sql
@@ -0,0 +1,14 @@
+-- Find the title of each film 
+SELECT Title FROM movies;
+
+-- Find the director of each film
+SELECT Director FROM movies;
+
+-- Find the title and director of each film
+SELECT Title,Director FROM movies;
+
+-- Find the title and year of each film
+SELECT Title,Year FROM movies;
+
+-- Find all the information about each film
+SELECT * FROM movies;
